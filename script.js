b112c999d4ccc904127094c3bbe9101a92d1eacf
APabloJ <altisimop@gmail.com>
2019-11-15 18:45:31 -0600

    added 'nextPlayer' and 'init' functions

--- script.js
+++ script.js
@@ -7,6 +7,25 @@ activePlayer = 0;
 dice = Math.floor(Math.random() * 6) + 1;
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
+      if(gamePlaying) {
+        // Random number
+        var dice = Math.floor(Math.random() * 6) + 1;
 
+        // Display the result
+        var diceDOM = document.querySelector('.dice');
+        diceDOM.style.display = 'block';
+        diceDOM.src = 'dice-' + dice + '.png';
+
+
+        // Update the round score IF the rolled number was NOT a 1
+        if (dice !== 1) {
+            //Add score
+            roundScore += dice;
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
+        } else {
+            // Next player
+            nextPlayer();
+        }
+    }      
 });
 
